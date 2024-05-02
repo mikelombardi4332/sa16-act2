b219aea1c23c68a92737da0b4c6a83d13badb40c
Michael Lombardi <mikelombardi4332@gmail.com>
2024-05-01 22:25:54 -0500

    sa16-act2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+    def home
+        render :home
+    end
+
+    def about
+        render :about
+    end
+
+    def work
+        render :work
+    end
+
+    def contact
+        render :contact
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root to: redirect('/home')
+  
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'work', to: 'pages#work', as: 'work'
+  get 'contact', to: 'pages#contact', as: 'contact'
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
